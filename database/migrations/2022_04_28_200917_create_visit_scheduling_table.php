ecfdd190dd619139ace9e96b5ca5e32f58f8f1b7
Pedro Lucas Guerra <pedrolucas.guerra28@gmail.com>
2022-07-06 13:24:43 -0300

    Arrumando problema de foreign key

--- database/migrations/2022_04_28_200917_create_visit_scheduling_table.php
+++ database/migrations/2022_04_28_200917_create_visit_scheduling_table.php
@@ -34,6 +34,6 @@ class CreateVisitSchedulingTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('visit_scheduling');
+        Schema::dropIfExists('visit_schedulings');
     }
 }
