8b9f872c7c8f84a20e16cc974c1f7e8a467c6bd1
Marcus <mfelipenovaes@hotmail.com>
2022-07-26 20:32:10 -0300

    Add default admin seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\User::create([
+            'name' => 'Admin',
+            'surname' => 'Teste',
+            'telephone' => '(35) 99999-9999',
+            'role' => '1',
+            'email' => 'admin@teste.com',
+            'password' => bcrypt( 'admin123' ),
+        ]);
+
         $this->call(UsersTableSeeder::class);
         $this->call(PropertiesTableSeeder::class);
         $this->call(VisitSchedulingTableSeeder::class);
