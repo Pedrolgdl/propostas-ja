8903d80846d1939c8fd16307108b030756e0e2a5
Marcus <mfelipenovaes@hotmail.com>
2022-08-17 16:50:39 -0300

    Add state em fillable

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -21,7 +21,7 @@ class Property extends Model
         'user_id', 'confirmed', 'type', 'title', 'description', 'price', 'iptu', 'size', 
         'number_rooms', 'number_bathrooms', 'furnished', 'disability_access', 'accepts_pet', 
         'garage', 'apartment_floor', 'condominium', 'condominium_description',  'fire_insurance', 
-        'service_charge', 'cep', 'city', 'neighborhood', 'street', 'house_number', 'nearby'
+        'service_charge', 'cep', 'state', 'city', 'neighborhood', 'street', 'house_number', 'nearby'
     ];
 
     // Conexão com tabela users
